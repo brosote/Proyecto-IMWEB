e38619d3073828d9321d07ff8005cccf2fd45865
brosote <92148822+brosote@users.noreply.github.com>
2022-03-04 09:16:39 +0100

    Add files via upload

---
+++ db_conn.php
@@ -0,0 +1,14 @@
+<?php
+	
+	$user= "root";
+	$pass = "";
+	$host= "localhost";
+	
+	$db_nombre = "web";
+
+	$connection = @mysqli_connect($host, $user, $pass, $db_nombre);
+
+	if (!$connection) {
+		echo "Connection failed!";
+	}
+?>
