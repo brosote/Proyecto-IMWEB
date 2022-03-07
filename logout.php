55ff48dd6212b007ce43630c92afe6571ac43374
brosote <92148822+brosote@users.noreply.github.com>
2022-03-07 20:15:44 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	session_start();
-	session_unset(); /*Libera todas las variables de sesión*/
-	session_destroy(); /*Destruye toda la información registrada de una sesión*/
-	header("Location: index.php"); /*Esto sirve para volver al encabezado de index.php*/
-?>
